9bae41bb0780070b40dfed77ae818bbd0478a44d
manuel <dragGH102@users.noreply.github.com>
2020-10-12 14:09:20 +0300

    correct next config file, optional catch route + imperative routing example

---
+++ src/pages/posts/comment/[[...optionalcatchallroute]].tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Layout from '@/components/Layout';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+export default function CatchAllPage(): JSX.Element {
+  const router = useRouter();
+  // /post/<id>/<comment>/[...more sub paths ...]?foo=abc
+  const { foo } = router.query;
+
+  const slug = router.query.catchallroute || [];
+
+  return (
+    <Layout>
+      <Head>
+        <title>a catch all route</title>
+      </Head>
+      <h1>
+        {/* @ts-ignore */}
+        Slug: {slug?.join('/')} - slug[0] : {slug[0]}
+      </h1>
+      <article>{`query param: ${foo}`}</article>
+    </Layout>
+  );
+}
